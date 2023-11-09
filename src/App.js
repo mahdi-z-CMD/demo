0c9a11b4152e899f03b0c7fcb7b92f3bf2d3a0da
mahdi <mahdi.zxzxz@gmail.com>
2023-11-09 19:57:37 +0330

    fixing Route

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
     <div className="home">
       <Routes>
         <Route path="/demo" element={<Home></Home>}></Route>
-        <Route path="demo/more" element={<More></More>}></Route>
-        <Route path="demo/login" element={<Login></Login>}></Route>
-        <Route path="demo/search" element={<Search></Search>}></Route>
-        <Route path='demo/company/:companyid' element={<Companypage></Companypage>}></Route>
+        <Route path="/demo/more" element={<More></More>}></Route>
+        <Route path="/demo/login" element={<Login></Login>}></Route>
+        <Route path="/demo/search" element={<Search></Search>}></Route>
+        <Route path='/demo/company/:companyid' element={<Companypage></Companypage>}></Route>
       </Routes>
       </div>
     </div>
