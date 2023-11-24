4e61566f3612f8dbadd97572d2732d8a9f3e401f
mahdi <mahdi.zxzxz@gmail.com>
2023-11-24 12:32:59 +0330

    testing Router

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
       <Navbar></Navbar>
     <div className="home">
       <Routes >
-        <Route path="/demo" element={<Home></Home>}></Route>
-        <Route path="/demo/more" element={<More></More>}></Route>
-        <Route path="/demo/login" element={<Login></Login>}></Route>
-        <Route path="/demo/search" element={<Search></Search>}></Route>
-        <Route path='/demo/company/:companyid' element={<Companypage></Companypage>}></Route>
-        <Route path='/demo/person/:personid' element={<Person></Person>}></Route>
+        <Route path="demo" element={<Home></Home>}></Route>
+        <Route path="demo/more" element={<More></More>}></Route>
+        <Route path="demo/login" element={<Login></Login>}></Route>
+        <Route path="demo/search" element={<Search></Search>}></Route>
+        <Route path='demo/company/:companyid' element={<Companypage></Companypage>}></Route>
+        <Route path='demo/person/:personid' element={<Person></Person>}></Route>
       </Routes>
       </div>
     </div>
